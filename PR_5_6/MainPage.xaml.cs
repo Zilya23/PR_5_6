ea3889866eae3ea4c0e6ce07f294a224cad61d94
Шагиахметова Зиля Рустемовна <201922@mck-ktits.ru>
2022-03-23 13:45:30 +0300

    Добавьте файлы проекта.

---
+++ PR_5_6/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace PR_5_6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public static ObservableCollection<Product> products { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>(bd_connection.aAAAEntities.Product.ToList());
+            var Prod = new Product();
+            this.DataContext = this;
+        }
+    }
+}
---
+++ PR_5_6/bd_connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_5_6
+{
+    class bd_connection
+    {
+        public static AAAAEntities1 aAAAEntities = new AAAAEntities1();
+    }
+}
